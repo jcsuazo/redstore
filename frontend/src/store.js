e6c1b847e8483e9698b44432b8bbc0c6f084d0e2
jcsuazo <jsuazo9@gmail.com>
2020-12-20 10:54:29 -0400

    add cart reducer and cart constants

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,13 +5,20 @@ import {
   productListReducer,
   productDETAILSReducer,
 } from './reducers/productReducers';
+import { cartReducer } from './reducers/cartReducers';
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDETAILSReducer,
+  cart: cartReducer,
 });
 
-const initialState = {};
+const cartItemsFromStorage = localStorage.getItem('cartItems')
+  ? JSON.parse(localStorage.getItem('cartItems'))
+  : [];
+const initialState = {
+  cart: { cartItems: cartItemsFromStorage },
+};
 
 const middleware = [thunk];
 const store = createStore(
