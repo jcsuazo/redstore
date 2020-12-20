ac3657b8f2f9b2530f675408fdfbecb3279a0039
jcsuazo <jsuazo9@gmail.com>
2020-12-20 17:27:19 -0400

    add registration logic to users controller and route on backend

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,27 @@
+import jwt from 'jsonwebtoken';
+import asyncHandler from 'express-async-handler';
+import User from '../models/userModel.js';
+
+const protect = asyncHandler(async (req, res, next) => {
+  let completeToken = req.headers.authorization;
+  let token;
+  if (completeToken && completeToken.startsWith('Bearer')) {
+    try {
+      token = completeToken.split(' ')[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = await User.findById(decoded.id).select('-password');
+      next();
+    } catch (error) {
+      console.error(error);
+      res.status(401);
+      throw new Error('Not authorized, token failed');
+    }
+  }
+
+  if (!token) {
+    res.status(401);
+    throw new Error('Not authorized, no token');
+  }
+});
+
+export { protect };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,25 @@
 import express from 'express';
-import { authUser } from '../controllers/userController.js';
+import {
+  authUser,
+  registerUser,
+  getUserProfile,
+} from '../controllers/userController.js';
+import { protect } from '../middleware/authMiddleware.js';
 const router = express.Router();
+
+// @desc    Register a new user
+// @route   POST /api/users
+// @access  Public
+router.route('/').post(registerUser);
 
 // @desc    Auth user & get token
 // @route   POST /api/user/login
 // @access  Public
 router.post('/login', authUser);
+
+// @desc    Get user profile
+// @route   GET /api/user/profile
+// @access  Private
+router.route('/profile').get(protect, getUserProfile);
 
 export default router;
