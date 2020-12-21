93f22ee7002e559f71387801194e5213dfeb0894
jcsuazo <jsuazo9@gmail.com>
2020-12-21 16:52:51 -0400

    add route to update order to be paid

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,5 +1,9 @@
 import express from 'express';
-import { addOrderItems, getOrderById } from '../controllers/orderController.js';
+import {
+  addOrderItems,
+  getOrderById,
+  updateOrderToPaid,
+} from '../controllers/orderController.js';
 import { protect } from '../middleware/authMiddleware.js';
 const router = express.Router();
 
@@ -12,5 +16,10 @@ router.route('/').post(protect, addOrderItems);
 // @route   GET /api/orders/:id
 // @access  Private
 router.route('/:id').get(protect, getOrderById);
+
+// @desc    Update order to paid
+// @route   GET /api/orders/:id/pay
+// @access  Private
+router.route('/:id/pay').put(protect, updateOrderToPaid);
 
 export default router;
