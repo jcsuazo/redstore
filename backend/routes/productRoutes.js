f12673dd3144912e0fe950dfb45a9c117def4d67
jcsuazo <jsuazo9@gmail.com>
2020-12-22 18:16:49 -0400

    add routes for create sample product and update a product by id

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,17 +4,23 @@ import {
   getProducts,
   getProductById,
   deleteProduct,
+  createProduct,
+  updateProduct,
 } from '../controllers/productController.js';
 const router = express.Router();
 
-// @desc    Fetch all products
-// @route   GET /api/products
-// @access  Public
-router.route('/').get(getProducts);
-
-// @desc    Fetch single product | Delete a product
-// @route   GET /api/products/:id | DELETE /api/products/:id
+// @desc    Fetch all products | Create a product
+// @route   GET /api/products | POST /api/products
 // @access  Public | Private/Admin
-router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct);
+router.route('/').get(getProducts).post(protect, admin, createProduct);
+
+// @desc    Fetch single product | Delete a product | Upate a product
+// @route   GET /api/products/:id | DELETE /api/products/:id | PUT /api/products/:id
+// @access  Public | Private/Admin | Private/Admin
+router
+  .route('/:id')
+  .get(getProductById)
+  .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct);
 
 export default router;
